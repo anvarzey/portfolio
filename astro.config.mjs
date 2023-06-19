b6decfcfc3f0792627678d819f7ddb19755c324a
Julián <anvarzey@gmail.com>
2023-06-19 13:54:47 +0200

    Added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import astroI18next from 'astro-i18next';
 
-// https://astro.build/config
 import image from '@astrojs/image';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroI18next(), image()]
+  integrations: [tailwind(), react(), astroI18next(), image()],
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  })
 });

279885dafa2678b6d15d3abab47b67cc71ac3682
Julián <anvarzey@gmail.com>
2023-06-24 00:36:33 +0200

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import astroI18next from 'astro-i18next';
 import image from '@astrojs/image';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), astroI18next(), image()],
-  output: "server",
+  output: "static",
   adapter: vercel({
     analytics: true
   })

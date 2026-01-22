4336e13f34c9535441ceabdeecf41d32ef34a707
julian <anvarzey@gmail.com>
2026-01-22 17:10:54 +0100

    enable vercel's web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,30 +4,29 @@ import astroI18next from 'astro-i18next';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-// import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), astroI18next()],
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'en'
-  },
-  output: "static",
-  // adapter: vercel({
-  //   analytics: true,
-  //   webAnalytics: {
-  //     enabled: true,
-  //   }
-  // }),
-  image: {
-    domains: ['res.cloudinary.com']
-    // service: sharpImageService()
-    // service: {
-    //   entrypoint: 'astro/assets/services/sharp',
-    // },
-  },
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	integrations: [react(), astroI18next()],
+	i18n: {
+		locales: ['en', 'es'],
+		defaultLocale: 'en'
+	},
+	output: "static",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		}
+	}),
+	image: {
+		domains: ['res.cloudinary.com']
+		// service: sharpImageService()
+		// service: {
+		//   entrypoint: 'astro/assets/services/sharp',
+		// },
+	},
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
